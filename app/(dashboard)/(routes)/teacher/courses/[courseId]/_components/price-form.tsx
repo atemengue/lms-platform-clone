b573cf907ce7c40038c99d82831d60a8b883bcb6
atemengue <moafembe@gmail.com>
2024-12-02 12:40:19 +0100

    course price form

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/price-form.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import * as z from 'zod';
+import axios from 'axios';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage
+} from "@/components/ui/form";
+import { Button } from '@/components/ui/button';
+import { Pencil } from 'lucide-react';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import { cn } from '@/lib/utils';
+import { Course } from '@prisma/client';
+import { Input } from '@/components/ui/input';
+import { formatPrice } from '@/lib/format';
+
+interface PriceFormProps  {
+  initialData: Course;
+  courseId: string;
+};
+
+const formSchema = z.object({
+  price: z.coerce.number(),
+})
+
+export const PriceForm = ({ initialData, courseId } : PriceFormProps) => {
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((current) => !current);
+
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { price: initialData?.price || undefined }
+  });
+
+  const { isSubmitted, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success("Course updated");
+      toggleEdit();
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong")
+    }
+  }
+
+  return(
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course Price
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing && (
+            <>Cancel</>
+          )}
+          {!isEditing && (
+            <>
+              <Pencil className='h-4  w-4 mr-2'/>
+              Edit Price
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <p className={cn(
+          "text-sm mt-2",
+          !initialData.price && "text-slate-500 italic"
+        )}>
+           {initialData.price ? formatPrice(initialData.price) : "No Price" }
+        </p> 
+      )}
+      {isEditing && (
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4 mt-43'>
+            <FormField control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input type='number' step="0.01" disabled={isSubmitted} placeholder='Set a price for your course' {...field}/>
+                </FormControl>
+                <FormMessage />
+              </FormItem> 
+            )} />
+            <div className='flex items-center gap-x-2'>
+              <Button disabled={!isValid || isSubmitted} type="submit">Save</Button>
+            </div>
+            </form>
+          </Form>
+        )}
+        </div>
+  )
+};
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,13 +1,14 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { LayoutDashboard } from "lucide-react";
+import { CircleDollarSign, LayoutDashboard, ListCheck } from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import * as React from 'react'
 import { CategoryForm } from "./_components/category-form";
+import { PriceForm } from "./_components/price-form";
 
 const CourseIdPage = async ({ params } : { params: { courseId: string }})=> {
 
@@ -72,6 +73,24 @@ const CourseIdPage = async ({ params } : { params: { courseId: string }})=> {
           label: category.name,
           value: category.id        
         })))} />
+      </div>
+      <div className="space-y-6">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={ListCheck} />
+            <h2 className="text-xl">Course chapters</h2>
+          </div>
+          <div>
+            TODO: Chapters
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={CircleDollarSign} />
+            <h2 className="text-xl">Sell your course</h2>
+          </div>
+          <PriceForm initialData={course} courseId={course.id} />
+        </div>
       </div>
     </div>
   </div> );
