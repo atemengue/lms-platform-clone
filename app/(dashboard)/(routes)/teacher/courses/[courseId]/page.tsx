b01d501f570453c3190d5700d81d409578b5a5b8
atemengue <moafembe@gmail.com>
2024-12-02 15:59:11 +0100

    Prisma schema finalization

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,7 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { CircleDollarSign, LayoutDashboard, ListCheck } from "lucide-react";
+import { CircleDollarSign, File, LayoutDashboard, ListCheck } from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
@@ -88,6 +88,13 @@ const CourseIdPage = async ({ params } : { params: { courseId: string }})=> {
           <div className="flex items-center gap-x-2">
             <IconBadge icon={CircleDollarSign} />
             <h2 className="text-xl">Sell your course</h2>
+          </div>
+          <PriceForm initialData={course} courseId={course.id} />
+        </div>
+        <div>
+        <div className="flex items-center gap-x-2">
+          <IconBadge icon={File} />
+          <h2 className="text-xl">Resources and Attachments</h2>
           </div>
           <PriceForm initialData={course} courseId={course.id} />
         </div>
